4afb2b9fe6c03bf73b610982844ebdbc6fdf315a
K-ITCE <khalid.itce@gmail.com>
2026-05-20 08:17:20 +0300

    Upload Circuit.png

--- src/secrets.h
+++ src/secrets.h
@@ -3,10 +3,10 @@
 #define SSID "TCL-5FE0-2.4GHz"
 #define PASSWORD "H4iu369dNpn6"
 
-#define SERVER_URL "[Public Server Address]/api/readings"
+#define SERVER_URL "https://esp32-rgms-oxln.onrender.com/api/readings"
 // Render example:
 // #define SERVER_URL "https://your-app-name.onrender.com/api/readings"
 
-#define SERVER_URL "[Localhost Address]/api/readings"
+// #define SERVER_URL "[Localhost Address]/api/readings"
 // Local example:
 // #define SERVER_URL "http://192.168.1.10:8000/api/readings"
