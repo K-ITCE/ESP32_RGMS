f21e1ef3257b0b4371554acc626f8972a20aa9e9
Khalid <khalid.itce@gmail.com>
2026-06-01 22:05:58 +0300

    Update secrets.h

--- src/secrets.h
+++ src/secrets.h
@@ -3,10 +3,6 @@
 #define SSID "[Network SSID]"
 #define PASSWORD "[Network Password]"
 
-#define SERVER_URL "[Public Server Address]/api/readings"
-// Render example:
-// #define SERVER_URL "https://your-app-name.onrender.com/api/readings"
-
-#define SERVER_URL "[Localhost Address]/api/readings"
-// Local example:
-// #define SERVER_URL "http://192.168.1.10:8000/api/readings"
+#define SERVER_URL "[Server Address]/api/readings"
+// Render (render.com) example: "https://your-app-name.onrender.com/api/readings"
+// Local example: "http://192.168.1.10:8000/api/readings"
